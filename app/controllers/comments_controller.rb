487fcaacd8eba790a292d1a84e1d448cff159936
denis281542 <deniskuimov@mail.ru>
2019-09-04 18:11:05 +0500

    Rendering Partial Collections

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
-  def 
-    @article = Article.find(params[:id])
+  def create
+    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)    
   end
 
   private
   def comment_params
-    params.require(:comments).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body)
   end
 end
