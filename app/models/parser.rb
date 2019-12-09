1dd814d3699ea12c20beed4da92328fdb420d69a
denis281542 <deniskuimov@mail.ru>
2019-12-09 23:12:49 +0500

    Edit model parser

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,2 +1,16 @@
 class Parser < ApplicationRecord
+  def parser
+  url = "https://hh.ru/search/vacancy?area=113&st=searchVacancy&text=ruby+on+rails&customDomain=1"
+  doc = Nokogiri::HTML(open(url))
+    doc.css(".vacancy-serp-item").each do |items|
+      vakancy = items.css(".HH-LinkModifier").text
+      salary = items.css(".vacancy-serp-item__compensation").text
+      date_of_publ= items.css(".vacancy-serp-item__publication-date").text 
+      unless salary.empty? 
+        puts "#{vakancy} - #{salary}" 
+        puts "#{date_of_publication}"
+        puts "======="
+      end 
+    end 
+  end
 end
