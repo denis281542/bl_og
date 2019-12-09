b4aa8b498a803cb582a2ce407229e552ff214906
denis281542 <deniskuimov@mail.ru>
2019-12-10 00:13:36 +0500

    Edit parser controller

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,4 +1,23 @@
 class Parser < ApplicationRecord
+
+  def parser
+    require 'openssl'
+    url = "https://hh.ru/search/vacancy?area=113&st=searchVacancy&text=ruby+on+rails&customDomain=1"
+    doc = Nokogiri::HTML(open(url))
+    
+    entries = doc.css('.lagos-market-rates-inner')
+    rate = entries.css('table')[0].css('tr')[1].css('td')[1].text
+    @formattedrate = rate[6..8]
+    render template: 'parallel/home'
+  end
+
+
+
+
+
+
+
+
   def parser
   url = "https://hh.ru/search/vacancy?area=113&st=searchVacancy&text=ruby+on+rails&customDomain=1"
   doc = Nokogiri::HTML(open(url))
@@ -8,7 +27,7 @@ class Parser < ApplicationRecord
       date_of_publ= items.css(".vacancy-serp-item__publication-date").text 
       unless salary.empty? 
         puts "#{vakancy} - #{salary}" 
-        puts "#{date_of_publication}"
+        puts "#{date_of_publ}"
         puts "======="
       end 
     end 
