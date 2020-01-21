f9ffb5f86c5db4579d0ac14b8dabce346bdf40bf
denis281542 <deniskuimov@mail.ru>
2020-01-21 09:26:31 +0500

    Edit form articles

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link_directory ..//= link_directory ../stylesheets .css
+/stylesheets .css
 //= link_directory ../stylesheets .css
